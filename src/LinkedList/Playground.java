840ac439c50a983593b64e16a27ac8e8419e1184
0xPunnawit <0xPunnawit.rod@gmail.com>
2025-08-10 00:29:31 +0700

    Singly Linked

---
+++ src/LinkedList/Playground.java
@@ -0,0 +1,97 @@
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playground {
+
+    public static void main(String[] args) {
+//        PNWJV สร้าง linked list
+        var head = new ListNode<>("HEAD");
+        var pNode = new ListNode<>("P");
+        var nNode = new ListNode<>("N");
+        var wNode = new ListNode<>("W");
+        var jNode = new ListNode<>("J");
+        var vNode = new ListNode<>("V");
+
+//        เชื่อมโยงแต่ละ Node (กำหนด next ของแต่ละ Node)
+        head.next = pNode;
+        pNode.next = nNode;
+        nNode.next = wNode;
+        wNode.next = jNode;
+        jNode.next = vNode;
+        System.out.println(traverseLinkedList(head.next));  // OUTPUT: HEAD->P->N->W->J->V
+
+//        สร้าง Node ใหม่ ที่ชื่อ A และเชื่อมกับ Node N
+        var aNode = new ListNode<>("A");
+        insertNext(nNode, aNode);
+        System.out.println(traverseLinkedList(head.next));  // OUTPUT: HEAD->P->N->A->W->J->V
+
+//        สร้าง Node ใหม่ ที่ชื่อ X และเชื่อมกับ Node J
+        var xNode = new ListNode<>("X");
+        insertBySearching(aNode, "J", xNode);
+        System.out.println(traverseLinkedList(head.next));  // OUTPUT: HEAD->P->N->A->W->J->X->V
+
+//        ลบ Node ที่ชื่อ A
+        deleteNode(head.next, "A");
+        System.out.println(traverseLinkedList(head.next));  // OUTPUT: HEAD->P->N->W->J->X->V
+
+    }
+
+    private static void deleteNode(ListNode<String> head, String targetData) {
+        ListNode<String> prev = head;                       //  ตัวชี้ไปยัง Node ก่อนหน้า
+        ListNode<String> curr = head.next;                  // ตัวชี้ไปยัง Node ปัจจุบัน
+        while (curr != null) {
+            if (curr.data.equals(targetData)) {
+                prev.next = curr.next;                      // เชื่อมโยง Node ก่อนหน้าไปยัง Node ถัดไป
+                curr.next = null;                           // Node ที่ถูกลบไม่ชี้ไปไหน
+                return;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+    }
+
+    private static void insertBySearching(ListNode<String> head, String searchTarget, ListNode<String> newNode) {
+        var currNode = head;
+        while (currNode != null) {
+            if (currNode.data.equals(searchTarget)) {
+                insertNext(currNode, newNode);
+            }
+                currNode = currNode.next;
+        }
+    }
+
+    private static void insertNext(ListNode<String> target, ListNode<String> newNode) {
+        ListNode<String> tmp = target.next;
+        target.next = newNode;
+        newNode.next = tmp;
+    }
+
+    private static String traverseLinkedList(ListNode<String> head) {
+        List<String> traverResult = new ArrayList<>();
+        var currNode = head;
+        while (currNode != null) {
+            traverResult.add(currNode.data);
+            currNode = currNode.next;
+        }
+        return String.join("->", traverResult);
+    }
+
+    private static class ListNode<T> {
+        public T data;
+        public ListNode<T> next;
+
+        public ListNode(T data) {
+            this.data = data;
+        }
+
+        public ListNode(T data, ListNode next) {
+            this.data = data;
+            this.next = next;
+        }
+
+
+    }
+
+}
